4aee9a35be59c96e43e808e93799c8f584a25ec3
Styopa Semenukha <ssemenukha@developmentgateway.org>
2018-01-12 16:47:53 -0500

    Find prefix

--- reposummary.py
+++ reposummary.py
@@ -9,6 +9,7 @@ import yaml
 class RepoList:
     def __init__(self):
         self._repos = {}
+        self._baseurls = set()
 
     def add_file(self, filename):
         base_name = os.path.basename(filename)
@@ -22,9 +23,14 @@ class RepoList:
             baseurls = ini.get(repoid, 'baseurl').split('\n')
             baseurls.sort()
             self._repos[repoid]['baseurl'] = baseurls[0]
+            self._baseurls.add(baseurls[0].split('/'))
 
     def __str__(self):
         return yaml.dump(self._repos, default_flow_style = False)
+
+    def find_prefix(self):
+        for i in range(0, len(self._baseurls[0]) - 1):
+            pass
 
 repo_list = RepoList()
 
